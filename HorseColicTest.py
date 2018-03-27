72417e5d75345fe65d517091aaa744cf545a7e00
Haotian <lihaotian1995@icloud.com>
2018-03-27 19:09:07 +0800

    version2.0
    add the gradAscent, compare two methods

--- HorseColicTest.py
+++ HorseColicTest.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# sigmoid function
+
 def sigmoid(inX):
     return 1.0/(1+np.exp(-inX))
 
@@ -8,26 +8,39 @@ def sigmoid(inX):
 def classifyVector(inX, weights):
     # use featureVector & weights to calculate the value of sigmoidFunction, and classify the result
     prob = sigmoid(sum(inX*weights))
-    print("\nprob: \n" + str(prob))
+    # print("\nprob: \n" + str(prob))
     if prob > 0.5:
         return 1.0
     else:
         return 0.0
 
 
+def gradAscent(dataMatrix, classLabels, numIter = 500):
+    m, n = np.shape(dataMatrix)
+    weights = np.ones(n)
+    alpha = 0.001
+
+    for j in range(numIter):
+        for i in range(m):
+            h = sigmoid(sum(dataMatrix[i] * weights))
+            error = classLabels[i] - h
+            weights = weights + alpha * error * dataMatrix[i]
+
+    return weights
+
+
 def improvedStocGradAscent(dataMatrix, classLabels, numIter = 150):
     m, n = np.shape(dataMatrix)
     weights = np.ones(n)
-    print("\nn: \n" + str(n))
     for j in range(numIter):
         dataIndex = range(m)
         for i in range(m):
-            alpha = 4/(1.0 + i + j) + 0.01
+            alpha = 4/(1.0 + i + j) + 0.001
             randomIndex = int(np.random.uniform(0, len(dataIndex)))
             chooseOne = dataIndex[randomIndex]
             h = sigmoid(sum(dataMatrix[chooseOne]*weights))
             error = classLabels[chooseOne] - h
-            weights = weights + alpha*error*dataMatrix[chooseOne]
+            weights = weights + alpha*error * dataMatrix[chooseOne]
             del(chooseOne)
 
     return weights
@@ -40,43 +53,51 @@ def horseTest():
     trainingLabels = []
     for line in frTrain.readlines():
         currLine = line.strip().split('\t')
+        # print("\ncurrLine: \n" + str(currLine))
         lineArr = []
         for i in range(21):
             lineArr.append(float(currLine[i]))
         trainingSet.append(lineArr)
         trainingLabels.append(float(currLine[21]))
 
-    trainingWeights = improvedStocGradAscent(np.array(trainingSet), trainingLabels, 500)
-    print("\ntrainingWeights: \n" + str(trainingWeights))
-    errorCount = 0
-    numTestVec = 0.0
+    trainingWeights1 = gradAscent(np.array(trainingSet), trainingLabels)
+    # print("\ntrainingWeights_gradAscent: \n" + str(trainingWeights1))
+    trainingWeights2 = improvedStocGradAscent(np.array(trainingSet), trainingLabels, 500)
+    # print("\ntrainingWeights_improvedStocGradAscent: \n" + str(trainingWeights2))
+
+    errorCount1 = 0
+    errorCount2 = 0
+    numTestVec = 0
 
     for line in frTest.readlines():
-        numTestVec += 1.0
+        numTestVec += 1
         currLine = line.strip().split('\t')
-        print("\ncurrLine: \n" + str(currLine))
+        # print("\ncurrLine: \n" + str(currLine))
         lineArr = []
         for i in range(21):
             lineArr. append(float(currLine[i]))
-        # if int(classifyVector(np.array(lineArr), trainingWeights)) != int(currLine[21]):
-        #     errorCount += 1
-            if sigmoid(np.array(lineArr[i])) != int(currLine[21]):
-                errorCount += 1
+        if int(classifyVector(np.array(lineArr), trainingWeights1)) != int(currLine[21]):
+            errorCount1 += 1
+        if int(classifyVector(np.array(lineArr), trainingWeights2)) != int(currLine[21]):
+            errorCount2 += 1
 
-    errorRate = (float(errorCount) / numTestVec)
-    print 'the error rate is %f' %  errorRate
-    return errorRate
+    errorRate1 = (float(errorCount1) / numTestVec)
+    print '\nthe error rate1 is %f' %  errorRate1
+    errorRate2 = (float(errorCount2) / numTestVec)
+    print 'the error rate2 is %f\n' % errorRate2
 
+    # return errorRate1
+    return errorRate2
 
 def multiTest():
     numTest = 10
     errorSum = 0.0
     for k in range(numTest):
         errorSum += horseTest()
-    print 'after %d iterations the average error is %f' % (numTest, errorSum / float(numTest))
+    print '\nafter %d iterations the average error is %f' % (numTest, errorSum / float(numTest))
 
 
 
 if __name__ == '__main__':
-    # multiTest()
-    horseTest()
+    multiTest()
+    # horseTest()
